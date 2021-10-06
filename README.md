082bace9b531c2f1b35579363b5e964ec9a1fa20
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-06 11:58:20 +0900

    the final version after the lecture

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # json-placeholder-test01
+
 Created with CodeSandbox
+
+# JSONPlaceholder を利用して
+
+JSON をとってきて表示するボタンを追加しただけのプロジェクト
--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickUser = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="App">
+        <button onClick={onClickUser}>users</button>
+        <button onClick={onClickUser1}> id=1のユーザ </button>
+      </div>
     </div>
   );
 }
